beb622ce9a0c8a6e5f83c8fce1d4d39397a16495
Sama Althiabat <althiabatsama@gmail.com>
2025-10-04 21:52:04 -0400

    Create svg-morph.js

---
+++ scripts/svg-morph.js
@@ -0,0 +1,19 @@
+const paths = [
+    'M20,20 L180,20 L180,180 L20,180 Z', // Square
+    'M100,20 L180,100 L100,180 L20,100 Z', // Diamond
+    'M100,20 C180,20 180,180 100,180 C20,180 20,20 100,20 Z' // Circle-like
+];
+
+let currentPath = 0;
+const morphBtn = document.getElementById('morph-btn');
+const pathElement = document.getElementById('morph-path');
+
+morphBtn.addEventListener('click', () => {
+    currentPath = (currentPath + 1) % paths.length;
+    anime({
+        targets: '#morph-path',
+        d: paths[currentPath],
+        duration: 1000,
+        easing: 'easeInOutQuad'
+    });
+});
