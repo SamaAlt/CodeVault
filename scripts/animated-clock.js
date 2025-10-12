b2aed2e442d49989bf7d0ba9ffc2b4f3dba94c61
Sama Althiabat <althiabatsamai@gmail.com>
2025-10-12 01:32:04 -0400

    Update animated-clock.js

--- scripts/animated-clock.js
+++ scripts/animated-clock.js
@@ -11,3 +11,14 @@ function updateClock() {
     document.querySelector('.second-hand').style.transform = `rotate(${secondDeg}deg)`;
     document.querySelector('.min-hand').style.transform = `rotate(${minuteDeg}deg)`;
     document.querySelector('.hour-hand').style.transform = `rotate(${hourDeg}deg)`;
+    
+    // Add smooth transition only when moving between minutes
+    if (seconds === 0) {
+        document.querySelectorAll('.hand').forEach(hand => {
+            hand.style.transition = 'all 0.05s cubic-bezier(0.1, 2.7, 0.58, 1)';
+        });
+    }
+}
+
+setInterval(updateClock, 1000);
+updateClock();
